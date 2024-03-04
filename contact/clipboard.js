dc0570fef9db74c3db443bfa574185f0678c9552
ThePhaseless <kukubaorch@gmail.com>
2024-03-04 23:53:36 +0000

    added Contact page with clipboard and popup

---
+++ contact/clipboard.js
@@ -0,0 +1,15 @@
+function copyDiscordUsernameToClipboard() {
+  console.log("Copying Discord username to clipboard");
+
+  // Get the text field
+  var copyText = "ThePhaseless";
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Get the popup
+  var popup = document.getElementById("myPopup");
+
+  // Toggle the show class
+  popup.classList.toggle("show");
+}
--- loading.js
+++
@@ -1,8 +0,0 @@
-// Show overlay when page is loading
-document.getElementById("myOverlay").style.display = "block";
-
-// Hide overlay when page is loaded
-window.addEventListener("load", function () {
-  document.getElementById("myOverlay").style.display = "none";
-  console.log("Page loaded");
-});
