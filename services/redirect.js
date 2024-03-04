51bbc10002c2e900fe7954b292f94e68a7fc7610
ThePhaseless <kukubaorch@gmail.com>
2024-03-04 21:45:55 +0000

    Update redirect URL in redirect.js

--- services/redirect.js
+++ services/redirect.js
@@ -1,7 +1,7 @@
-window.onload = function() {
-    var currentURL = window.location.href;
-    var domain = currentURL.split('/')[2]; // Extract the domain from the URL
-    if (currentURL.includes("/services")) {
-        window.location.href = "http://services." + domain;
-    }
-}
+window.onload = function () {
+  var currentURL = window.location.href;
+  var domain = currentURL.split("/")[2]; // Extract the domain from the URL
+  if (currentURL.includes("/services")) {
+    window.location.href = "http://dashboard." + domain;
+  }
+};
